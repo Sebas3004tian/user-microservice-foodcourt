a7eee8d85ac5793b572087ccfed6aafc2383a50c
Sebastian Lopez <sebas3004tian@gmail.com>
2026-03-18 14:07:27 -0500

    chore: initialize spring boot project

---
+++ src/main/java/com/foodcourt/user_microservice_foodcourt/UserMicroserviceFoodcourtApplication.java
@@ -0,0 +1,13 @@
+package com.foodcourt.user_microservice_foodcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroserviceFoodcourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceFoodcourtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodcourt/user_microservice_foodcourt/UserMicroserviceFoodcourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcourt.user_microservice_foodcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceFoodcourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
