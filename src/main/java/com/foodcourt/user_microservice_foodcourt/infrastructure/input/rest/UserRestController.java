acaf6d217955458182dff20bf9101c26f466b400
Sebastian Lopez <sebas3004tian@gmail.com>
2026-03-19 17:48:37 -0500

    docs: create a new owner is done

--- src/main/java/com/foodcourt/user_microservice_foodcourt/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/foodcourt/user_microservice_foodcourt/infrastructure/input/rest/UserRestController.java
@@ -2,6 +2,9 @@ package com.foodcourt.user_microservice_foodcourt.infrastructure.input.rest;
 
 import com.foodcourt.user_microservice_foodcourt.application.dto.request.CreateOwnerRequestDto;
 import com.foodcourt.user_microservice_foodcourt.application.handler.IUserHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,12 @@ public class UserRestController {
     private final IUserHandler userHandler;
 
     @PostMapping("/owner")
+    @Operation(summary = "Create an owner user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data"),
+            @ApiResponse(responseCode = "409", description = "User already exists")
+    })
     public ResponseEntity<Void> createOwner(@Valid @RequestBody CreateOwnerRequestDto ownerRequestDto){
         userHandler.createOwner(ownerRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
